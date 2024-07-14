7dc127e6b64413d2a974da777c99db9197409b44
Sean Bahrami <sbahr001@gmail.com>
2024-07-14 03:10:48 -0700

    Cleanup

--- src/Jobs/ProcessDataJob.php
+++ src/Jobs/ProcessDataJob.php
@@ -8,8 +8,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Str;
-use Ramsey\Uuid\Uuid;
 
 class ProcessDataJob implements ShouldQueue
 {
